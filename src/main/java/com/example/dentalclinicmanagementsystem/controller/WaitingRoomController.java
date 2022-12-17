7a489fe63cfe0e316826906bb32e46a6df595310
xuanduc1506 <nxduc150600@gmail.com>
2022-12-18 01:04:22 +0700

    add websocket

--- src/main/java/com/example/dentalclinicmanagementsystem/controller/WaitingRoomController.java
+++ src/main/java/com/example/dentalclinicmanagementsystem/controller/WaitingRoomController.java
@@ -7,7 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -24,11 +29,30 @@ public class WaitingRoomController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private SimpMessagingTemplate template;
+
     @PostMapping("call-patient/{id}")
     public ResponseEntity<Void> callPatient(@PathVariable Long id) {
         waitingRoomService.callPatient(id);
         kafkaTemplate.send("waiting-room", id.toString());
         return ResponseEntity.ok().build();
+    }
+
+    @KafkaListener(
+            topics = "waiting-room",
+            groupId = "dental"
+    )
+    public void listen(String id) {
+        System.out.println("sending via kafka listener..");
+        template.convertAndSend("/topic/group", id);
+    }
+
+    @MessageMapping("/call-patient")
+    @SendTo("/topic/group")
+    public String broadcastGroupMessage(@Payload String id) {
+        //Sending this message to all the subscribers
+        return id;
     }
 
     @GetMapping("get-list-waiting")
